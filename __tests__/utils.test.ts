68462d1d224f6b583a586ad9314f09f29ec12611
thomaszdxsn <thomaszdxsn@icloud.com>
2024-01-16 21:00:44 +0800

    :white_check_mark: Introduce jest to project
    and adjust some file & folder structure

---
+++ __tests__/utils.test.ts
@@ -0,0 +1,16 @@
+import { formatSeconds } from "../src/utils";
+
+
+it("formatSeconds", () => {
+  expect(formatSeconds(0)).toBe("00:00:00")
+  expect(formatSeconds(1)).toBe("00:00:01")
+  expect(formatSeconds(59)).toBe("00:00:59")
+  expect(formatSeconds(60)).toBe("00:01:00")
+  expect(formatSeconds(61)).toBe("00:01:01")
+  expect(formatSeconds(3599)).toBe("00:59:59")
+  expect(formatSeconds(3600)).toBe("01:00:00")
+  expect(formatSeconds(3601)).toBe("01:00:01")
+  expect(formatSeconds(86399)).toBe("23:59:59")
+  expect(formatSeconds(86400)).toBe("24:00:00")
+  expect(formatSeconds(86401)).toBe("24:00:01")
+})
