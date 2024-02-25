b39ca54c8fbf3e4715ac710d423ee8d9d04da874
thomaszdxsn <thomaszdxsn@icloud.com>
2024-02-26 04:26:46 +0800

    :bug: Fix todayTimeEntriesAtom bug, add `isToday` util

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,25 +1,30 @@
 import { TimeEntry } from "src/interfaces";
-import { formatSeconds, nowPercentageInDay, isSameTimer, formatTime } from "../src/utils";
-
+import {
+  formatSeconds,
+  nowPercentageInDay,
+  isSameTimer,
+  formatTime,
+  isToday,
+} from "../src/utils";
 
 it("formatSeconds", () => {
-  expect(formatSeconds(0)).toBe("00:00:00")
-  expect(formatSeconds(1)).toBe("00:00:01")
-  expect(formatSeconds(59)).toBe("00:00:59")
-  expect(formatSeconds(60)).toBe("00:01:00")
-  expect(formatSeconds(61)).toBe("00:01:01")
-  expect(formatSeconds(3599)).toBe("00:59:59")
-  expect(formatSeconds(3600)).toBe("01:00:00")
-  expect(formatSeconds(3601)).toBe("01:00:01")
-  expect(formatSeconds(86399)).toBe("23:59:59")
-  expect(formatSeconds(86400)).toBe("24:00:00")
-  expect(formatSeconds(86401)).toBe("24:00:01")
-})
+  expect(formatSeconds(0)).toBe("00:00:00");
+  expect(formatSeconds(1)).toBe("00:00:01");
+  expect(formatSeconds(59)).toBe("00:00:59");
+  expect(formatSeconds(60)).toBe("00:01:00");
+  expect(formatSeconds(61)).toBe("00:01:01");
+  expect(formatSeconds(3599)).toBe("00:59:59");
+  expect(formatSeconds(3600)).toBe("01:00:00");
+  expect(formatSeconds(3601)).toBe("01:00:01");
+  expect(formatSeconds(86399)).toBe("23:59:59");
+  expect(formatSeconds(86400)).toBe("24:00:00");
+  expect(formatSeconds(86401)).toBe("24:00:01");
+});
 
 it("nowPercentageInDay", () => {
-  expect(nowPercentageInDay()).toBeGreaterThan(0)
-  expect(nowPercentageInDay()).toBeLessThan(1)
-})
+  expect(nowPercentageInDay()).toBeGreaterThan(0);
+  expect(nowPercentageInDay()).toBeLessThan(1);
+});
 
 it("isSameTimer", () => {
   const timer = {
@@ -28,7 +33,7 @@ it("isSameTimer", () => {
     tagIds: [1, 2, 3],
     projectName: "123",
     tags: ["123", "456", "789"],
-  }
+  };
   const entry: TimeEntry = {
     project_id: 1,
     description: "test",
@@ -48,22 +53,32 @@ it("isSameTimer", () => {
     uid: 1,
     user_id: 1,
     workspace_id: 1,
-    wid: 1
-  }
-  expect(isSameTimer({ timer, entry })).toBeTruthy()
-})
-
+    wid: 1,
+  };
+  expect(isSameTimer({ timer, entry })).toBeTruthy();
+});
 
 describe("formatTime", () => {
   it("default format is HH:mm", () => {
-    expect(formatTime("2021-11-24 01:02:03")).toBe("01:02")
-  })
+    expect(formatTime("2021-11-24 01:02:03")).toBe("01:02");
+  });
 
   it("invalid datetime format is --", () => {
-    expect(formatTime("invalid")).toBe("--")
-  })
+    expect(formatTime("invalid")).toBe("--");
+  });
 
   it("custom format", () => {
-    expect(formatTime("2021-11-24 01:02:03", "HH:mm:ss")).toBe("01:02:03")
-  })
-})
+    expect(formatTime("2021-11-24 01:02:03", "HH:mm:ss")).toBe("01:02:03");
+  });
+});
+
+describe("isToday", () => {
+  it("1998-08-01 is not today", () => {
+    expect(isToday("1998-08-01")).toBeFalsy();
+  });
+
+  it("new Date() is today", () => {
+    const todayISO = new Date().toISOString();
+    expect(isToday(todayISO)).toBeTruthy();
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,181 +1,185 @@
-import { Notice, Plugin, WorkspaceLeaf } from 'obsidian';
-import { SettingTab } from './plugin/setting-tab';
-import { TogglService } from './toggl-apis';
-import { TogglView, VIEW_TYPE_TOGGL } from './plugin/views'
-import { currentEntryProjectAtom, passedTimeAtom, savedTimersAtom, store, tick } from './atoms';
-import { Timer } from './interfaces'
-import { produce } from 'immer'
-import { TimerListModal } from './plugin/modal';
+import { Notice, Plugin, WorkspaceLeaf } from "obsidian";
+import { SettingTab } from "./plugin/setting-tab";
+import { TogglService } from "./toggl-apis";
+import { TogglView, VIEW_TYPE_TOGGL } from "./plugin/views";
+import {
+  currentEntryProjectAtom,
+  passedTimeAtom,
+  savedTimersAtom,
+  store,
+  tick,
+} from "./atoms";
+import { Timer } from "./interfaces";
+import { produce } from "immer";
+import { TimerListModal } from "./plugin/modal";
 
 interface PluginSettings {
-	apiToken: string;
-	showInStatusBar: boolean
+  apiToken: string;
+  showInStatusBar: boolean;
 }
-
 
 export default class TogglPlugin extends Plugin {
-	public settings: PluginSettings;
-	public togglService: TogglService;
-	private statusBarItem: HTMLElement | null = null
-	private tickInterval: NodeJS.Timer | null = null
-	private statusUnsubscriber: (() => void) | null = null
-	private savedTimersUnsubscriber: (() => void) | null = null
-	private pluginData: PluginData;
+  public settings: PluginSettings;
+  public togglService: TogglService;
+  private statusBarItem: HTMLElement | null = null;
+  private tickInterval: NodeJS.Timer | null = null;
+  private serviceTickInterval: NodeJS.Timer | null = null;
+  private statusUnsubscriber: (() => void) | null = null;
+  private savedTimersUnsubscriber: (() => void) | null = null;
+  private pluginData: PluginData;
 
-	async onload() {
-		this.pluginData = new PluginData(this);
-		await Promise.all([
-			this.loadSavedTimers(),
-			this.loadSettings()
-		])
-		this.addSettingTab(new SettingTab(this.app, this));
-		this.startTick()
-		this.showStatusBarItem()
-		this.registerView(
-			VIEW_TYPE_TOGGL,
-			(leaf) => new TogglView(leaf, this)
-		)
-		this.addRibbonIcon("dice", 'Activate Toggl View', () => {
-			this.initView()
-		})
-		this.addCommand({
-			id: "start-timer",
-			name: "Start Timer",
-			callback: () => {
-				new TimerListModal(this.app, this).open()
-			}
-		})
+  async onload() {
+    this.pluginData = new PluginData(this);
+    await Promise.all([this.loadSavedTimers(), this.loadSettings()]);
+    this.addSettingTab(new SettingTab(this.app, this));
+    this.startTick();
+    this.showStatusBarItem();
+    this.registerView(VIEW_TYPE_TOGGL, (leaf) => new TogglView(leaf, this));
+    this.addRibbonIcon("dice", "Activate Toggl View", () => {
+      this.initView();
+    });
+    this.addCommand({
+      id: "start-timer",
+      name: "Start Timer",
+      callback: () => {
+        new TimerListModal(this.app, this).open();
+      },
+    });
 
+    if (this.app.workspace.layoutReady) {
+      await this.initView();
+    } else {
+      this.registerEvent(
+        this.app.workspace.on("layout-change", this.initView.bind(this)),
+      );
+    }
+  }
 
-		if (this.app.workspace.layoutReady) {
-			await this.initView();
-		} else {
-			this.registerEvent(
-				this.app.workspace.on("layout-change", this.initView.bind(this))
-			);
-		}
-	}
+  onunload() {
+    if (this.tickInterval) {
+      clearInterval(this.tickInterval);
+    }
+    if (this.savedTimersUnsubscriber) {
+      this.savedTimersUnsubscriber();
+    }
+    if (this.serviceTickInterval) {
+      clearInterval(this.serviceTickInterval);
+    }
+  }
 
-	onunload() {
-		if (this.tickInterval) {
-			clearInterval(this.tickInterval)
-		}
-		if (this.savedTimersUnsubscriber) {
-			this.savedTimersUnsubscriber()
-		}
-	}
+  async loadSettings() {
+    this.settings = await this.pluginData.loadSettings();
+  }
 
-	async loadSettings() {
-		this.settings = await this.pluginData.loadSettings()
-	}
+  async loadSavedTimers() {
+    const timers = await this.pluginData.loadTimers();
+    store.set(savedTimersAtom, timers);
+    this.savedTimersUnsubscriber = store.sub(savedTimersAtom, () => {
+      const timers = store.get(savedTimersAtom);
+      this.pluginData.saveTimers(timers);
+    });
+  }
 
-	async loadSavedTimers() {
-		const timers = await this.pluginData.loadTimers()
-		store.set(savedTimersAtom, timers)
-		this.savedTimersUnsubscriber = store.sub(savedTimersAtom, () => {
-			const timers = store.get(savedTimersAtom)
-			this.pluginData.saveTimers(timers)
-		})
-	}
+  async saveSettings() {
+    await this.pluginData.saveSettings(this.settings);
+  }
 
-	async saveSettings() {
-		await this.pluginData.saveSettings(this.settings);
-	}
+  async initView() {
+    const { workspace } = this.app;
+    let leaf: WorkspaceLeaf;
+    const leaves = workspace.getLeavesOfType(VIEW_TYPE_TOGGL);
 
-	async initView() {
-		const { workspace } = this.app
-		let leaf: WorkspaceLeaf
-		const leaves = workspace.getLeavesOfType(VIEW_TYPE_TOGGL)
+    if (leaves.length > 0) {
+      leaf = leaves[0];
+    } else {
+      leaf = workspace.getRightLeaf(false);
+      await leaf.setViewState({
+        type: VIEW_TYPE_TOGGL,
+        active: true,
+      });
+    }
 
-		if (leaves.length > 0) {
-			leaf = leaves[0]
-		} else {
-			leaf = workspace.getRightLeaf(false)
-			await leaf.setViewState({
-				type: VIEW_TYPE_TOGGL,
-				active: true
-			})
-		}
+    workspace.revealLeaf(leaf);
+  }
 
-		workspace.revealLeaf(leaf)
-	}
+  startTick() {
+    this.togglService = new TogglService(this.settings.apiToken);
+    this.serviceTickInterval = this.togglService.tick();
+    this.tickInterval = tick();
+  }
 
-	startTick() {
-		this.togglService = new TogglService(this.settings.apiToken);
-		this.togglService.tick();
-		this.tickInterval = tick()
-	}
+  showStatusBarItem() {
+    const statusBarItem = this.addStatusBarItem();
+    this.statusBarItem = statusBarItem;
+    this.statusUnsubscriber = store.sub(passedTimeAtom, () => {
+      const passedTime = store.get(passedTimeAtom);
+      if (!passedTime) {
+        statusBarItem.setText("--");
+        return;
+      }
+      const projectName = store.get(currentEntryProjectAtom)?.name ?? "";
+      const text = [projectName, passedTime].filter(Boolean).join(" ");
+      statusBarItem.setText(text);
+    });
+  }
 
-	showStatusBarItem() {
-		const statusBarItem = this.addStatusBarItem();
-		this.statusBarItem = statusBarItem
-		this.statusUnsubscriber = store.sub(passedTimeAtom, () => {
-			const passedTime = store.get(passedTimeAtom)
-			if (!passedTime) {
-				statusBarItem.setText("--")
-				return
-			}
-			const projectName = store.get(currentEntryProjectAtom)?.name ?? ""
-			const text = [projectName, passedTime].filter(Boolean).join(" ")
-			statusBarItem.setText(text)
-		})
-	}
-
-	hideStatusBarItem() {
-		this.statusBarItem?.remove()
-		this.statusBarItem = null
-		this.statusUnsubscriber?.()
-		this.statusUnsubscriber = null
-	}
+  hideStatusBarItem() {
+    this.statusBarItem?.remove();
+    this.statusBarItem = null;
+    this.statusUnsubscriber?.();
+    this.statusUnsubscriber = null;
+  }
 }
 
-
 interface Data {
-	settings?: PluginSettings;
-	savedTimers?: Timer[]
+  settings?: PluginSettings;
+  savedTimers?: Timer[];
 }
 
 class PluginData {
-	plugin: TogglPlugin;
-	static DEFAULT_SETTINGS: PluginSettings = {
-		apiToken: "",
-		showInStatusBar: true
-	}
+  plugin: TogglPlugin;
+  static DEFAULT_SETTINGS: PluginSettings = {
+    apiToken: "",
+    showInStatusBar: true,
+  };
 
-	constructor(plugin: TogglPlugin) {
-		this.plugin = plugin;
-	}
+  constructor(plugin: TogglPlugin) {
+    this.plugin = plugin;
+  }
 
-	async getData(): Promise<Data> {
-		return (this.plugin.loadData().then(data => data ?? {})) as Promise<Data>
-	}
+  async getData(): Promise<Data> {
+    return this.plugin.loadData().then((data) => data ?? {}) as Promise<Data>;
+  }
 
-	async saveData(data: Data): Promise<void> {
-		await this.plugin.saveData(data);
-	}
+  async saveData(data: Data): Promise<void> {
+    await this.plugin.saveData(data);
+  }
 
-	async saveSettings(settings: PluginSettings): Promise<void> {
-		await this.saveData(produce(await this.getData(), draft => {
-			draft.settings = settings
-			return draft
-		}))
-	}
+  async saveSettings(settings: PluginSettings): Promise<void> {
+    await this.saveData(
+      produce(await this.getData(), (draft) => {
+        draft.settings = settings;
+        return draft;
+      }),
+    );
+  }
 
-	async saveTimers(timers: Timer[]): Promise<void> {
-		await this.saveData(produce(await this.getData(), draft => {
-			draft.savedTimers = timers
-			return draft
-		}))
+  async saveTimers(timers: Timer[]): Promise<void> {
+    await this.saveData(
+      produce(await this.getData(), (draft) => {
+        draft.savedTimers = timers;
+        return draft;
+      }),
+    );
+  }
 
-	}
+  async loadSettings(): Promise<PluginSettings> {
+    const data = await this.getData();
+    return data.settings ?? PluginData.DEFAULT_SETTINGS;
+  }
 
-	async loadSettings(): Promise<PluginSettings> {
-		const data = await this.getData();
-		return data.settings ?? PluginData.DEFAULT_SETTINGS;
-	}
-
-	async loadTimers(): Promise<Timer[]> {
-		const data = await this.getData();
-		return data?.savedTimers ?? [];
-	}
+  async loadTimers(): Promise<Timer[]> {
+    const data = await this.getData();
+    return data?.savedTimers ?? [];
+  }
 }
